984526925304af89908c400788b102f833f605be
Diego Vicuña <diego704249@gmail.com>
2021-10-10 15:51:20 -0500

    agregado entidades

---
+++ PlatziCSharp/Entidades/Escuela.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatziCSharp.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+        
+        public string Nombre
+        {
+            get { return "copia: " + nombre;}
+            set { nombre = value.ToUpper(); }
+        }
+
+        public int AñoDeCreacion { get; set;}
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+
+        public string TipoEscuela { get; set; }
+
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+
+    }
+}
---
+++ PlatziCSharp/Objetos/Program.cs
@@ -0,0 +1,16 @@
+﻿using PlatziCSharp.Entidades;
+using System;
+
+namespace PlatziCSharp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi ",2012); ;
+            escuela.Nombre = "Platzi ";
+            Console.WriteLine(escuela.Nombre);
+        }
+    }
+}
