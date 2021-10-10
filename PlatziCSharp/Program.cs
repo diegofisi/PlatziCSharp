b554513792dd166c6c484e6299299f2eafcabc32
Diego Vicuña <diego704249@gmail.com>
2021-10-10 15:08:30 -0500

    Agregar archivos de proyecto.

---
+++ PlatziCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PlatziCSharp
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo = "Freddy Vega";
+
+        public void Timbrar()
+        {
+            //Todo
+            Console.Beep(2000, 3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Platzi";
+            miEscuela.direccion = "Cr 9 calle 72";
+            miEscuela.añoFundacion = 2012;
+            Console.WriteLine("Timbre");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
